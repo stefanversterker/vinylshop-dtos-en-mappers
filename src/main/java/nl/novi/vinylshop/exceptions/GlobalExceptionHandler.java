563547fe80bf3c4b339e4c041f346b8f8bd7e734
stefanversterker <stefanversterker@gmail.com>
2026-05-27 19:29:43 +0200

    af

--- src/main/java/nl/novi/vinylshop/exceptions/GlobalExceptionHandler.java
+++ src/main/java/nl/novi/vinylshop/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package nl.novi.vinylshop.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,7 +17,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public List<String> handleException(MethodArgumentNotValidException ex){
-        return ex.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+    public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<String> violations = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+        return ResponseEntity.badRequest().body(violations);
     }
+
+
 }
